287dd97b66c585670cde7f67dd06378fb7a96c36
Artemii0056 <aza_art@mail.ru>
2021-10-19 00:59:53 +0500

    123

--- Scripts/PlayerHealth.cs
+++ Scripts/PlayerHealth.cs
@@ -5,27 +5,21 @@ using UnityEngine.Events;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public event UnityAction HealthChanged;
+
     private float _current = 0;
-
-    [SerializeField] private UnityEvent _sliderChanger;
-
-    public event UnityAction SliderChanger
-    {
-        add => _sliderChanger.AddListener(value);
-        remove => _sliderChanger.RemoveListener(value);
-    }
 
     public float Current => _current;
 
     public void OnButtonClickDecrease()
     {
         _current -= 10f;
-        _sliderChanger?.Invoke();
+        HealthChanged?.Invoke();
     }
 
     public void OnButtonClickIncrease()
     {
         _current += 10f;
-        _sliderChanger?.Invoke();    
+        HealthChanged?.Invoke();    
     }
 }
--- Scripts/SliderChanger.cs
+++ Scripts/SliderChanger.cs
@@ -8,29 +8,31 @@ using UnityEngine.UI;
 public class SliderChanger : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
-
-    private PlayerHealth _health;
-    private float _value; 
-
-    private void Update() 
-    {        
-        float maxDelta = 10f;
-        _slider.value = Mathf.MoveTowards(_slider.value, _value, maxDelta * Time.deltaTime);
-    }
-
-    private void Change()
-    {
-        _value = _health.Current;
-    }
+    [SerializeField] private PlayerHealth _player;
 
     private void OnEnable()
     {
-        _health = GetComponent<PlayerHealth>();
-        _health.SliderChanger += Change;
+        _player.HealthChanged += OnSliderChanger;
     }
 
     private void OnDisable()
     {
-        _health.SliderChanger -= Change;
+        _player.HealthChanged -= OnSliderChanger;
+    }
+
+    private void OnSliderChanger()
+    {
+        StartCoroutine(Changer());
+        StopCoroutine(Changer());
+    }
+
+    private IEnumerator Changer()
+    {
+        while (_slider.value != _player.Current)
+        {
+            float maxDelta = 10f;
+            _slider.value = Mathf.MoveTowards(_slider.value, _player.Current, maxDelta * Time.deltaTime);
+            yield return null;
+        }
     }
 }
